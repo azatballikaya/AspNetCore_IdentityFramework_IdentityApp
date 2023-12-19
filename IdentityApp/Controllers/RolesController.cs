869e72610d850688ada301bb48264cc719700367
azatballikaya <azat.bal@hotmail.com>
2023-12-19 22:44:15 +0300

    RolesController and Views has been added

---
+++ IdentityApp/Controllers/RolesController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using IdentityApp.Models;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace IdentityApp.Controllers {
+    public class RolesController:Controller{
+        private readonly RoleManager<AppRole> _roleManager;
+        public RolesController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager=roleManager;
+        }
+        public IActionResult Index(){
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create(){
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(AppRole model){
+
+            if(ModelState.IsValid){
+                var result=await _roleManager.CreateAsync(model);
+                if(result.Succeeded){
+                    return RedirectToAction("Index");
+                }
+                foreach(var item in result.Errors){
+                    ModelState.AddModelError("",item.Description);
+                }
+
+            }
+            
+                return View(model);
+            
+        }
+    }
+}
--- IdentityApp/Controllers/UsersController.cs
+++ IdentityApp/Controllers/UsersController.cs
@@ -39,5 +39,51 @@ namespace IdentityApp.Controllers{
             return View(model);
 
         }
+        [HttpGet]
+        public async Task<IActionResult> Edit(string? id){
+            if(id==null)
+            return NotFound();
+            var user=await _userManager.FindByIdAsync(id);
+            if(user==null)
+            return NotFound();
+            var update=new EditViewModel {
+                UserId=user.Id,
+                FullName=user.FullName,
+                Email=user.Email,
+                
+            };
+            return View(update);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditViewModel model){
+            if(ModelState.IsValid){
+                var user=await _userManager.FindByIdAsync(model.UserId);
+                if(user==null)
+                return NotFound();
+                user.Email=model.Email;
+                user.FullName=model.FullName;
+                var result=await _userManager.UpdateAsync(user);
+                if(result.Succeeded && model.Password!=null){
+                    await _userManager.RemovePasswordAsync(user);
+                    await _userManager.AddPasswordAsync( user,model.Password);
+                }
+                
+                if(result.Succeeded){
+                    return RedirectToAction("Index");
+                }
+                foreach(var err in result.Errors){
+                    ModelState.AddModelError("",err.Description);
+                }
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(string? id){
+            var user=await _userManager.FindByIdAsync(id);
+            if(user!=null){
+                await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        } 
     }
 }
